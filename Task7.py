d2ab48f82ffdf11e1cf1a8bca29de49dd3e4409c
Danya2kk <dkorz.33@gmail.com>
2021-10-19 22:47:24 +0300

    Finished Task7

--- Task7.py
+++ Task7.py
@@ -1,6 +1,21 @@
-word = input("Введите слово: ")
+print("Привет, давай поиграем в поросячую латынь)")
 
-vowels = ['a', 'e', 'i', 'o', 'u', 'y']
 suf1 = "way"
-if word == word.startswith('a', 'e', 'i') or word.startswith('o', 'u', 'y'):
-    print(word[1:] + word[0] + suf1)
+suf2 = "ay"
+while True:
+    word = input("Введи слово на английском(ЭТО ВАЖНО!!!) или нажмите q для выхода:  ")
+    if word == 'q':
+        break
+    if word.startswith('a') or word.startswith('e') or word.startswith('i') \
+            or word.startswith('u') or word.startswith('y') or word.startswith('o'):
+         print(word[1:] + word[0] + suf1)
+    else:
+        print(word[1:] +word[0] + suf2)
+    continue
+
+print("Хорошего дня, пока!")
+
+
+
+
+
