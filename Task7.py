32377149dabb6a36d077ed3605a7146bd2e27e8a
Danya2kk <dkorz.33@gmail.com>
2021-10-19 22:21:34 +0300

    Added to Task7

--- Task7.py
+++ Task7.py
@@ -2,5 +2,5 @@ word = input("Введите слово: ")
 
 vowels = ['a', 'e', 'i', 'o', 'u', 'y']
 suf1 = "way"
-if word == word.startswith("a", "e", "i", "o", "u", "y"):
+if word == word.startswith('a', 'e', 'i') or word.startswith('o', 'u', 'y'):
     print(word[1:] + word[0] + suf1)
